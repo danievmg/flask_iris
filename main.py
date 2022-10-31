87cf44625b16def001087694da2ca3a22d0e6d8f
DANIEL CRISTIANO FRANCO SILVA <13938046678>
2022-10-31 08:59:47 -0300

    Fist commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
 def index():
-    return jsonify({"Mensagem": "Daniel Cristiano"})
+    return jsonify({"Mensagem": "Daniel a"})
 
 
 if __name__ == '__main__':
